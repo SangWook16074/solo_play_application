dc98144b8fa7619ad5447bc3c7606fbe72fd69c3
WantOscar <kimdw3824@gmail.com>
2025-01-17 20:06:57 +0900

    파일 이름 appView로 선택

---
+++ lib/src/views/app_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppState();
+}
+
+class _AppState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
