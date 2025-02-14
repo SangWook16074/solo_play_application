02d1248bd1af4b4fc4ab2ee75eabb787211c3aa9
WantOscar <kimdw3824@gmail.com>
2025-02-14 17:09:15 +0900

    main 파일에서 go router 따로 파일 분리함

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:solo_play_application/src/app/presentation/page/app_page.dart';
-import 'package:solo_play_application/src/course/presentation/view/course_detail_view.dart';
-import 'package:solo_play_application/src/post/presentation/view/posting_detail_view.dart';
+import 'package:solo_play_application/src/route/router.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-final GoRouter _router = GoRouter(
-  routes: [
-    /// 앱 실행 시 첫 화면 : home화면
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const AppPage(),
-    ),
-    GoRoute(
-      path: '/postingDetailView',
-      builder: (context, state) => const PostingDetailView(),
-    ),
-    GoRoute(
-      path: '/courseDetailView',
-      builder: (context, state) => const CourseDetailView(),
-    ),
-  ],
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -32,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _router,
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
     );
   }
