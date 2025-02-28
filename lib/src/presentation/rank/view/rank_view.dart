9af8f161154b1308c02b02f9ff12bfa0bf2761b6
WantOscar <kimdw3824@gmail.com>
2025-02-28 21:45:17 +0900

    PostingDetailView 및 MyPostingWidget 코드 리펙토링 후 rank view 작업 시작

--- lib/src/presentation/rank/view/rank_view.dart
+++ lib/src/presentation/rank/view/rank_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solo_play_application/src/core/style/theme_color.dart';
 
 class RankView extends StatelessWidget {
   const RankView({super.key});
@@ -6,8 +7,38 @@ class RankView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
+      appBar: _appbar(),
+      body: CustomScrollView(
+        slivers: [
+          _ranking(),
+        ],
+      ),
+    );
+  }
+
+  /// appbar 영역
+  PreferredSize _appbar() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(65),
+      child: AppBar(
+        title: const Text(
+          '이번 주 가장 인기있는 장소',
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black87),
+        ),
+      ),
+    );
+  }
+
+  Widget _ranking() {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 275,
+        width: 390,
+        decoration: BoxDecoration(
+          color: ThemeColor.basicGrey,
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
