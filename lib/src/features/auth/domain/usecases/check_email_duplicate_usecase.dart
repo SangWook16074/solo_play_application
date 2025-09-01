b265bb860ababdc38f66851532de1160abb5b3c1
sangwook16074 <qazws78941@gmail.com>
2025-09-01 14:55:33 +0900

    사용자 이메일 중복 검사 유스케이스 제작

--- lib/src/features/auth/domain/usecases/check_email_duplicate_usecase.dart
+++ lib/src/features/auth/domain/usecases/check_email_duplicate_usecase.dart
@@ -1,17 +1,19 @@
-import 'package:solo_play_application/src/features/auth/domain/entities/email_duplicate_status.dart';
+import 'package:solo_play_application/src/core/utils/networks/result.dart';
 import 'package:solo_play_application/src/features/auth/domain/repositories/auth_repository.dart';
 
 final class CheckEmailDuplicateUsecaseImpl
     implements CheckEmailDuplicateUsecase {
   final AuthRepository _authRepository;
 
-  const CheckEmailDuplicateUsecaseImpl(this._authRepository);
+  const CheckEmailDuplicateUsecaseImpl({
+    required AuthRepository authRepository,
+  }) : _authRepository = authRepository;
   @override
-  Future<EmailDuplicateStatus> call(String email) async {
-    return _authRepository.checkEmailDuplicate(email).then((response) {});
+  Future<Result<String>> call(String email) async {
+    return _authRepository.checkEmailDuplicate(email);
   }
 }
 
 abstract class CheckEmailDuplicateUsecase {
-  Future<EmailDuplicateStatus> call(String email);
+  Future<Result<String>> call(String email);
 }
---
+++ test/feature/auth/domain/usecases/check_email_duplicate_usecase_test.dart
@@ -0,0 +1,52 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:solo_play_application/src/core/utils/networks/result.dart';
+import 'package:solo_play_application/src/features/auth/domain/repositories/auth_repository.dart';
+import 'package:solo_play_application/src/features/auth/domain/usecases/check_email_duplicate_usecase.dart';
+import 'package:test/test.dart';
+
+class MockAuthRepository extends Mock implements AuthRepository {}
+
+void main() {
+  group(CheckEmailDuplicateUsecase, () {
+    late MockAuthRepository mockAuthRepository;
+    late CheckEmailDuplicateUsecase checkEmailDuplicateUsecase;
+
+    setUp(() {
+      mockAuthRepository = MockAuthRepository();
+      checkEmailDuplicateUsecase =
+          CheckEmailDuplicateUsecaseImpl(authRepository: mockAuthRepository);
+    });
+
+    test('should return correctly when success', () async {
+      final email = "test@test.com";
+      when(
+        () => mockAuthRepository.checkEmailDuplicate(email),
+      ).thenAnswer((_) async => Success("사용 가능한 이메일입니다!"));
+
+      final result = await checkEmailDuplicateUsecase.call(email);
+
+      verify(
+        () => mockAuthRepository.checkEmailDuplicate(email),
+      ).called(1);
+
+      expect(result, isA<Success>());
+      expect((result as Success).value, "사용 가능한 이메일입니다!");
+    });
+
+    test('should return correctly when failure', () async {
+      final email = "test@test.com";
+      when(
+        () => mockAuthRepository.checkEmailDuplicate(email),
+      ).thenAnswer((_) async => Failure("이미 있는 아이디에요."));
+
+      final result = await checkEmailDuplicateUsecase.call(email);
+
+      verify(
+        () => mockAuthRepository.checkEmailDuplicate(email),
+      ).called(1);
+
+      expect(result, isA<Failure>());
+      expect((result as Failure).message, "이미 있는 아이디에요.");
+    });
+  });
+}
