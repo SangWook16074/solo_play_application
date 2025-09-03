5b3e8852a950f2d4d1be52956257afc18437f03d
sangwook16074 <qazws78941@gmail.com>
2025-09-03 13:09:51 +0900

    feature/auth 중 불필요한 소스코드 제거

--- lib/src/features/auth/domain/repositories/auth_repository.dart
+++ lib/src/features/auth/domain/repositories/auth_repository.dart
@@ -1,8 +1,6 @@
 import 'package:solo_play_application/src/core/utils/networks/result.dart';
-import 'package:solo_play_application/src/features/auth/data/models/check_email_duplicate_dto.dart';
 import 'package:solo_play_application/src/features/auth/data/models/sign_in_dto.dart';
 import 'package:solo_play_application/src/features/auth/data/models/sign_up_dto.dart';
-import 'package:solo_play_application/src/features/auth/domain/entities/email_duplicate_status.dart';
 
 abstract class AuthRepository {
   Future<SignInResponseDto> signIn(SignInDto signIn);
